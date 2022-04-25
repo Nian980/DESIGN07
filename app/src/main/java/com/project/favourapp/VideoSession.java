0664d365778ed08f98499a1a75a513e42e61a87d
Nianxu Wang <nianxuwang@berkeley.edu>
2022-04-24 21:23:57 -0700

    popup menu

--- app/src/main/java/com/project/favourapp/VideoSession.java
+++ app/src/main/java/com/project/favourapp/VideoSession.java
@@ -3,12 +3,51 @@ package com.project.favourapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class VideoSession extends AppCompatActivity {
+
+    ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_session);
+
+        // Referencing and Initializing the button
+        button = (ImageButton) findViewById(R.id.settingsBtn);
+
+        // Setting onClick behavior to the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initializing the popup menu and giving the reference as current context
+                // popupMenu is our settings menu!
+                PopupMenu popupMenu = new PopupMenu(VideoSession.this, button);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.settings_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(VideoSession.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+
+                        // Put case statements here to do things depending on menu choice
+
+
+                    }
+                });
+
+                // Showing the popup menu
+                popupMenu.show();
+            }
+        });
     }
 }
