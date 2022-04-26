d0f0f7765769ff65ec905cd25a86102d1ba4a877
Nianxu Wang <nianxuwang@berkeley.edu>
2022-04-25 23:01:58 -0700

    adding video

--- app/src/main/java/com/project/favourapp/VideoSession.java
+++ app/src/main/java/com/project/favourapp/VideoSession.java
@@ -2,32 +2,39 @@ package com.project.favourapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
-import android.widget.Toast;
+
+// Credits for sample lecture video:
+// https://www.youtube.com/watch?v=xbpUHSKoALg
 
 public class VideoSession extends AppCompatActivity {
 
-    ImageButton button;
+    ImageButton settingsButton;
+    ImageButton camButton;
+    ImageButton screenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_session);
 
-        // Referencing and Initializing the button
-        button = (ImageButton) findViewById(R.id.settingsBtn);
+        // Referencing and Initializing the buttons
+        settingsButton = (ImageButton) findViewById(R.id.settingsBtn);
+        camButton = (ImageButton) findViewById(R.id.camBtn);
+        screenButton = (ImageButton) findViewById(R.id.screenBtn);
 
-        // Setting onClick behavior to the button
-        button.setOnClickListener(new View.OnClickListener() {
+        // Setting onClick behavior to the settingsButton
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Initializing the popup menu and giving the reference as current context
                 // popupMenu is our settings menu!
-                PopupMenu popupMenu = new PopupMenu(VideoSession.this, button);
+                PopupMenu popupMenu = new PopupMenu(VideoSession.this, settingsButton);
 
                 // Inflating popup menu from popup_menu.xml file
                 popupMenu.getMenuInflater().inflate(R.menu.settings_menu, popupMenu.getMenu());
@@ -36,12 +43,11 @@ public class VideoSession extends AppCompatActivity {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
                         // Toast message on menu item clicked
-                        Toast.makeText(VideoSession.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-                        return true;
+//                        Toast.makeText(VideoSession.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
 
                         // Put case statements here to do things depending on menu choice
 
-
+                        return true;
                     }
                 });
 
@@ -49,5 +55,22 @@ public class VideoSession extends AppCompatActivity {
                 popupMenu.show();
             }
         });
+
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start VideoSession activity if we click the cam button
+                Intent i = new Intent(VideoSession.this, VideoSession.class);
+                startActivity(i);
+            }
+        });
+
+        // Write a listener for screenButton to change to screen sharing mode (create another activity!)
+
+        // Implement whiteboard capabilities for the whiteboard button?
+
+
+
+
     }
 }
